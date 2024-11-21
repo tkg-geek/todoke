1c0b1d8958e325a7123647a57fca06c1f3a06640
tkg-geek <tkg.geek@gmail.com>
2024-11-21 10:04:24 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include 'includes/head.php'; ?>
+
+<body>
+    <div class="container centered">
+    <img src="img/todoke_logo.svg" alt="TODOKE Logo" class="logo-img">
+        <a href="post.php" class="button">声を届ける</a>
+        <a href="read.php" class="button">声に耳を傾ける</a>
+    </div>
+    
+    <!-- Particle background -->
+    <div id="particles-js"></div>
+    <script src="https://cdn.jsdelivr.net/npm/particles.js"></script>
+    <script src="js/particle.js"></script>
+</body>
+</html>
